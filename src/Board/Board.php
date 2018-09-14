690c2b2a95260dec2c97be16144bae63965fc093
TimiTao <kunicki.tomasz@gmail.com>
2018-09-14 17:27:02 +0200

    update interface

--- src/Board/Board.php
+++ src/Board/Board.php
@@ -6,11 +6,8 @@ namespace Aggrego\Domain\Board;
 
 use Aggrego\Domain\Profile\Profile;
 use Aggrego\EventStore\Aggregate;
-use Aggrego\EventStore\Uuid;
 
 interface Board extends Aggregate
 {
-    public function getUuid(): Uuid;
-
     public function getProfile(): Profile;
 }
