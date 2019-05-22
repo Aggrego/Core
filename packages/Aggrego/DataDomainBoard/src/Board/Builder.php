48ecb235b426364bcd66b78288e84f9df86738f9
TimiTao <kunicki.tomasz@gmail.com>
2019-05-22 18:01:04 +0200

    Add monorepo basic files

--- packages/Aggrego/DataDomainBoard/src/Board/Builder.php
+++ packages/Aggrego/DataDomainBoard/src/Board/Builder.php
@@ -15,13 +15,14 @@ namespace Aggrego\DataDomainBoard\Board;
 
 use Aggrego\DataDomainBoard\Board\Board as DataBoard;
 use Aggrego\DataDomainBoard\Board\Prototype\Board as DataBoardPrototype;
+use Aggrego\DataDomainBoard\Board\Prototype\Metadata as DataBoardMetadata;
 use Aggrego\Domain\Board\Board as DomainBoard;
 use Aggrego\Domain\Board\Builder as FactoryInterface;
 use Aggrego\Domain\Board\Exception\UnsupportedPrototypeBuilderException;
 use Aggrego\Domain\Board\Key;
-use Aggrego\Domain\Board\Prototype\Metadata as DomainMetadata;
 use Aggrego\Domain\Board\Prototype\Board as PrototypeBoard;
 use Aggrego\Domain\Board\Prototype\Metadata;
+use Aggrego\Domain\Board\Prototype\Metadata as DomainMetadata;
 use Aggrego\Domain\Board\Uuid;
 use Aggrego\Domain\Profile\Profile;
 use Assert\Assertion;
@@ -34,14 +35,27 @@ class Builder implements FactoryInterface
         return $board instanceof DataBoardPrototype;
     }
 
-    public function build(Uuid $uuid, Key $key, Profile $profile, DomainMetadata $metadata, ?Uuid $parentUuid): DomainBoard
-    {
+    /**
+     * @param Uuid $uuid
+     * @param Key $key
+     * @param Profile $profile
+     * @param DomainMetadata|DataBoardMetadata $metadata
+     * @param Uuid|null $parentUuid
+     * @return DomainBoard
+     */
+    public function build(
+        Uuid $uuid,
+        Key $key,
+        Profile $profile,
+        DomainMetadata $metadata,
+        ?Uuid $parentUuid
+    ): DomainBoard {
         try {
-            Assertion::isInstanceOf($metadata, Metadata::class);
+            Assertion::isInstanceOf($metadata, DataBoardMetadata::class);
         } catch (AssertionFailedException $e) {
             throw new UnsupportedPrototypeBuilderException($e->getMessage(), $e->getCode(), $e);
         }
-        /** @var Metadata $metadata */
+        /** @var DataBoardMetadata $metadata */
         return new DataBoard($uuid, $key, $profile, $metadata, $parentUuid);
     }
 }
